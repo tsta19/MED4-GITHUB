391f3ba72fc4425b5c8235027bbb7da673aa5e23
Herrelaekker <bocaj317@gmail.com>
2021-03-17 13:04:57 +0100

    skrevet kommentarer og har lavet det pænere i konsollen

--- MED4/evaluation.py
+++ MED4/evaluation.py
@@ -10,12 +10,18 @@ from sklearn.metrics import plot_confusion_matrix
 from sklearn.model_selection import cross_val_score
 import numpy as np
 
+
+#Funktion som returnerer en string an på hvilket 'target' det har.
 def target_type(row):
     if row['Target'] == 0:
         return 'Voice'
     elif row['Target'] == 1:
         return 'Noise'
 
+
+"""Et library der består af et antal arrays, som hver er opdelt på følgende: [x,y]
+   Her kan x f.eks. være en pitch-værdi, for et enkelt lydsample.
+   Her vil y være den kategori den er blevet klassificeret som. Eks. 0 eller 1 -> 'Voice' eller 'Noice'"""
 dataLibrary = [[1,0],[3,0],[2,0],[5,0],[1,0],[1,0],[1,0],[7,1],[8,1],[9,1],[11,1],[29,1],[19,1],[8,1],[6,1],
         [3,0],[1,0],[1,0],[5,0],[1,0],[4,0],[5,0],[7,1],[8,1],[9,1],[11,1],[29,1],[19,1],[8,1],[10,1],
         [5,0],[2,0],[1,0],[2,0],[4,0],[3,0],[5,0],[7,1],[8,1],[9,1],[11,1],[29,1],[19,1],[8,1],[9,1],
@@ -23,51 +29,95 @@ dataLibrary = [[1,0],[3,0],[2,0],[5,0],[1,0],[1,0],[1,0],[7,1],[8,1],[9,1],[11,1
         [7,0],[3,0],[2,0],[6,0],[1,0],[1,0],[4,0],[5,1],[8,1],[19,1],[11,1],[29,1],[19,1],[45,1],[5,1],
         [4,0],[4,0],[2,0],[3,0],[1,0],[1,0],[4,0],[5,1],[18,1],[9,1],[11,1],[29,1],[19,1],[8,1],[1,1]]
 
+
+# Laver DataFrame hvilket inddeler det i kolonner og rækker. (Se det første som bliver printet.)
 df = pd.DataFrame(dataLibrary)
-df.columns = ['Pitch', 'Target']
+df.columns = ['Pitch', 'Target'] # Giver en overskrift til hver kolonne i dataframen.
+
+# Laver en ny kolonne kaldet 'Type', hvor hver værdi får tildelt enten 'Voice' eller 'Noise (ud fra om 'Target' er
+# enten 0 eller 1.).
 df['Type'] = df.apply(target_type, axis=1)
-df.head()
+
+# df.head(5) #<- returnerer de 5 første rækker.
+
 print(df)
 
-X = df[df.columns[0:1]].values
-Y = df[df.columns[1]].values
+X = df[df.columns[0:1]].values # X indeholder alle værdier i første række.
+Y = df[df.columns[1]].values # Y indeholder alle værdier i anden række.
 
+
+# Splitter datassættet op så 20 % bliver testet på mens resten (80 %) bliver trænet på.
+# x_train indeholder altså alle X-værdier som bliver trænet på. x_test indeholder de X-værdier som bliver testet på.
+# Det samme gør sig gældende for y_train og y_test, bare med y-værdierne i stedet.
+# Dette gør at vi får en ny opdeling hver gang vi starter programmet, og fjerner noget bias.
 x_train,  x_test, y_train, y_test = model_selection.train_test_split(X,Y,test_size=0.2)
 
 
-knn_clf = KNeighborsClassifier(n_neighbors=3)
-knn_clf.fit(x_train, y_train)
-knn_predictions = knn_clf.predict(x_test)
-print(x_test)
-print(knn_predictions)
+knn_clf = KNeighborsClassifier(n_neighbors=3) # Laver en K-NeighborsClassifier (Metoden)
+knn_clf.fit(x_train, y_train) # Putter træningssættet ind i classifieren. Træner ud fra træningssættet.
+knn_predictions = knn_clf.predict(x_test) # Predicter hvad alle testresultaterne burde være ud fra træningen.
+
+# De næste 9 linjer kode gør bare at x_test og knn_predictions bliver printet pænere i konsollen.
+str_x_test = ""
+for i in range(len(x_test)):
+    str_x_test += str(x_test[i])
+print('x_test:        ' + str_x_test)
+
+str_knn_predictions = ""
+for i in range(len(knn_predictions)):
+    str_knn_predictions += "[" + str(knn_predictions[i]) + "]"
+print("Predicted y's: " + str(str_knn_predictions))
+# ---------------
+
+# Beregner en accuracy score ud fra hvor mange predictions den har fået rigtige.
 print('Accuracy of the knn algorithm is {}'.format(accuracy_score(y_test,knn_predictions)))
-print('Cross Validation Accuracy of the knn algorithm is {}'.format(cross_val_score(knn_clf, x_train, y_train, cv=5).mean()))
+# Cross validation beregner accuracy scoren men ud fra at den tester 5 gange (Prøv at slette .mean() og se dens output).
+print('Cross Validation Accuracy of the knn algorithm is {}'.format(cross_val_score(knn_clf, X, Y, cv=5).mean()))
 
+# Her predicter den hvad en 5'er ville svare til i y.
 prediction = knn_clf.predict(np.array([[5]]))
-print('Knn predicts [5] to be ' +str(prediction))
+print(f'Knn predicts [5] to be ' + str(prediction))
 
+# Printer en matrix i konsollen.
+print("Confusion Matrix:")
 print(confusion_matrix(y_test, knn_predictions))
+# Laver en matrix som kan blive plottet (ud fra KNeighborsClassifieren). Den plotter testdataen.
 matrix = plot_confusion_matrix(knn_clf, x_test,y_test, cmap=plt.cm.Reds)
-matrix.ax_.set_title('Confusion Matrix', color='white')
-plt.xlabel('Predicted label', color='white')
-plt.ylabel('True label', color='white')
-#-------------fjollet, start
-#plt.gcf().axes[0].tick_params(colors='white')
-#plt.gcf().axes[1].tick_params(colors='white')
-#plt.gcf().set_size_inches(10,6)
-#-------------fjollet, stop
-plt.show()
+matrix.ax_.set_title('K-Nearest Neighbors', color='black')
+plt.xlabel('Predicted label', color='black')
+plt.ylabel('True label', color='black')
 
+
+# Gør de samme ting som med knn-classifieren så bare med en ny classifier
 dt_clf = DecisionTreeClassifier()
-dt_clf.fit(x_train,y_train)
+dt_clf.fit(x_train, y_train)
 dt_predictions = dt_clf.predict(x_test)
-print(dt_predictions)
 print('Accuracy of the dt algorithm is {}'.format(accuracy_score(y_test,dt_predictions)))
 print('Cross Validation Accuracy of the dt algorithm is {}'.format(cross_val_score(dt_clf, x_train, y_train, cv=5).mean()))
+prediction = dt_clf.predict(np.array([[5]]))
+print(f'dt predicts [5] to be ' + str(prediction))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, dt_predictions))
+matrix = plot_confusion_matrix(dt_clf, x_test,y_test, cmap=plt.cm.Reds)
+matrix.ax_.set_title('Decision Tree', color='black')
+plt.xlabel('Predicted label', color='black')
+plt.ylabel('True label', color='black')
 
+
+# Gør de samme ting som med knn-classifieren så bare med en ny classifier
 nb_clf = GaussianNB()
-nb_clf.fit(x_train,y_train)
-nb_predictions = dt_clf.predict(x_test)
-print(nb_predictions)
+nb_clf.fit(x_train, y_train)
+nb_predictions = nb_clf.predict(x_test)
 print('Accuracy of the nb algorithm is {}'.format(accuracy_score(y_test,nb_predictions)))
 print('Cross Validation Accuracy of the nb algorithm is {}'.format(cross_val_score(nb_clf, x_train, y_train, cv=5).mean()))
+prediction = nb_clf.predict(np.array([[5]]))
+print(f'nb predicts [5] to be ' + str(prediction))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, nb_predictions))
+matrix = plot_confusion_matrix(nb_clf, x_test,y_test, cmap=plt.cm.Reds)
+matrix.ax_.set_title('Gaussian NB', color='black')
+plt.xlabel('Predicted label', color='black')
+plt.ylabel('True label', color='black')
+
+
+plt.show()

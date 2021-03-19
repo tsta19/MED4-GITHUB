8f94cdd7c6f21fdbd419f1aa17c9beb8fc139cfd
tsta19 <tsta19@student.aau.dk>
2021-03-19 08:48:43 +0100

    Added DataManager and Sound Files

---
+++ MED4/DataManager.py
@@ -0,0 +1,121 @@
+import math
+import os
+from tqdm import tqdm
+import time
+import numpy as np
+import wave
+import matplotlib.pyplot as plt
+import warnings
+
+
+
+class DataManager:
+    # Directory Settings
+    testDirectory = ""
+    savePath = ""
+    processedDirectory = "Sorted_Mic_Sample_Values_Processed"
+    soundDirectory = "Mic_Samples"
+    soundGraphDirectory = "Sorted_Mic_Sample_Graphs"
+
+    # Variables
+    executionTime = time.time()
+    iteration = 0
+    soundIteration = 1
+    resultIteration = 1
+    txtFileType = ".txt"
+    soundFileType = ".wav"
+    specifySoundFile = "mic_sample_"
+
+    # Sound Related
+    sampleRate = 44100
+    warnings.simplefilter("ignore", DeprecationWarning)
+
+    def writeTextFile(self, name, value):
+        fileName = os.path.join(self.savePath, name + ".txt")
+        txt = open(fileName, "w+")
+        txt.write(value)
+
+    def dataManager(self):
+        dataMangerCode = "Function: dataManager"
+        userInput = input("Image Directory:" + "\n")
+        if userInput == "smsv":
+            self.testDirectory = "Sorted_Mic_Sample_Values"
+        else:
+            self.testDirectory = userInput
+        userInput = input("Save Path:" + "\n")
+        if userInput == "smsvp":
+            self.savePath = "Sorted_Mic_Sample_Values_Processed"
+        else:
+            self.savePath = userInput
+        print("+-------------------------+")
+        print("| Data Processing Started.")
+        print("| Directory:", self.testDirectory, )
+        print("| Save Path:", self.savePath)
+        print("+-------------------------+")
+        print("| Data in Directory:", len(os.listdir(self.testDirectory)))
+        print("+-------------------------+")
+        for data in tqdm(os.listdir(self.testDirectory)):
+            if data.endswith(".txt"):
+                print(data)
+                self.iteration += 1
+                self.writeTextFile("Processed_" + str(self.iteration), "Processed")
+            else:
+                print("No DATA was found", "ERROR: Wrong filetype")
+        print(dataMangerCode, "operations are Done")
+
+    def printTestingResults(self):
+        printResultsCode = "Function: printTestingResults"
+        print("+-------- Test Results --------+")
+        print("Images tested:", self.iteration)
+        print("Execution time: %s" % round((time.time() - self.executionTime), 2), "seconds")
+        print("+-------- DATA EVALUATION ---------+")
+
+        for data in tqdm(os.listdir(self.processedDirectory)):
+
+            processedFiles = os.path.join(self.processedDirectory, "Processed_" + str(self.resultIteration) + ".txt")
+            print("********* " + "Processed_" + str(self.resultIteration) + ".txt" + " *********")
+            if data.endswith(".txt"):
+                with open(processedFiles, "r") as dataFile:
+                    print(dataFile.readlines())
+            else:
+                print("No DATA was found", "ERROR: Wrong filetype")
+            self.resultIteration += 1
+
+        print("+------------------------------+")
+        print(printResultsCode, "operations are Done")
+
+    def soundDataManager(self):
+        soundFileCode = "Function: soundDataManager"
+        for soundFiles in tqdm(os.listdir(self.soundDirectory)):
+            soundFilesSpec = os.path.join(self.soundDirectory, str(self.specifySoundFile) + str(self.soundIteration) + ".wav")
+            if soundFiles.endswith(".wav"):
+                soundFile = wave.open(soundFilesSpec, "r")
+                da = np.fromstring(soundFile.readframes(self.sampleRate), dtype=np.int16)
+                left, right = da[0::2], da[1::2]
+                lf, rf = abs(np.fft.rfft(left)), abs(np.fft.rfft(right))
+                plt.figure(self.soundIteration)
+                a = plt.subplot(211)
+                r = 2 ** 16 / 2
+                a.set_ylim([-r, r])
+                a.set_xlabel('time [s]   ' + str(self.specifySoundFile) + str(self.soundIteration))
+                a.set_ylabel('sample value [-]')
+                x = np.arange(22050) / 22050
+                plt.plot(x, left)
+                b = plt.subplot(212)
+                b.set_xscale('log')
+                b.set_xlabel('frequency [Hz]   ' + str(self.specifySoundFile) + str(self.soundIteration))
+                b.set_ylabel('|amplitude|')
+                plt.plot(lf)
+                plt.tight_layout()
+                plt.savefig(str(self.soundGraphDirectory) + "/" + str(self.specifySoundFile) + str(self.soundIteration) + '.png')
+                self.soundIteration += 1
+            else:
+                print("ERROR: Wrong filetype" + "\n", "It is supposed to be ", self.soundFileType)
+        print(soundFileCode, "operations are Done")
+
+
+if __name__ == '__main__':
+    dm = DataManager()
+    # dm.dataManager()
+    # dm.printTestingResults()
+    dm.soundDataManager()

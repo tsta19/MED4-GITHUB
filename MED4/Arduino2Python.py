359b0b1bf6bbfb9dea4396978b065b3b23944364
Sebastian <sebber280@live.dk>
2021-03-26 13:24:06 +0100

    Now plots samplesize after collecting

--- MED4/Arduino2Python.py
+++ MED4/Arduino2Python.py
@@ -17,12 +17,25 @@ class SensorDataManager:
 
     def plotData(self):
         plt.ion()
-        plt.ylim(0, 100)
+        plt.ylim(int(np.min(self.dataArray)-5), int(np.max(self.dataArray)+5))
         plt.grid(True)
         plt.plot(self.dataArray, '-', label="Decibel (dB)")
 
+    def drawPlot(self):
+        drawnow(self.plotData)
 
-    def updatePlot(self):
+    def writeToCSV(self, filename, data):
+        fileName = f"{filename}.csv"
+        file = open(fileName, "w")
+        print(f"Opened file: {fileName} for writing")
+        print("Writing...")
+        file.write(str(data))
+        print("Writing stopped")
+        file.close()
+        print(f"{self.sampleSize} samples saved to file: {fileName}")
+
+    def collectData(self):
+
         while self.arduino.inWaiting() == 0:
             pass
         print("Collecting sensor data...")
@@ -31,24 +44,15 @@ class SensorDataManager:
             decode = data.decode("utf-8")
             decodedData = int(decode)
             self.dataArray.append(decodedData)
-            drawnow(self.plotData)
             self.counter += 1
-        print("Done collecting")
-        sensorCollector.writeToCSV("test")
 
-    def writeToCSV(self, filename):
-        fileName = f"{filename}.csv"
-        file = open(fileName, "w")
-        print(f"Opened file: {fileName} for writing")
-        print("Writing...")
-        file.write(str(self.dataArray))
-        print("Writing stopped")
-        file.close()
-        print(f"{self.sampleSize} samples saved to file: {fileName}")
+        return self.dataArray
 
 
 if __name__ == "__main__":
-    sensorCollector = SensorDataManager("COM3", 115200, 50)
+    sensorCollector = SensorDataManager("COM3", 9600, 1000)
+    sensorCollector.writeToCSV("test", sensorCollector.collectData())
     sensorCollector.plotData()
-    sensorCollector.updatePlot()
+    sensorCollector.drawPlot()
+
 
